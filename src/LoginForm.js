92c6ddb38246a5b4b96a2c63e364691fc0b81210
Emerson Ribeiro <emersonrjr03@gmail.com>
2021-02-14 17:47:14 +0000

    removing login fields, because we have no account creation yet

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -20,39 +20,46 @@ function LoginForm() {
 
     }
 
+    const accountCreationImplemented = false;
+
     return (
         <div className="login_form__content">
-            <Form>
-                <Form.Group as={Row} controlId="formPlaintextEmail">
-                    <Form.Label column sm="3" >Email</Form.Label>
-                    <Col sm="9">
-                        <Form.Control type="email" placeholder="name@example.com" />
-                    </Col>
-                </Form.Group>
+            {accountCreationImplemented ? 
+                (<Form>
+                    <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="3" >Email</Form.Label>
+                        <Col sm="9">
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Col>
+                    </Form.Group>
 
-                <Form.Group as={Row} controlId="formPlaintextPassword">
-                    <Form.Label column sm="3">
-                    Password
-                    </Form.Label>
-                    <Col sm="9">
-                    <Form.Control type="password" placeholder="Password" />
-                    </Col>
-                </Form.Group>
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="3">
+                        Password
+                        </Form.Label>
+                        <Col sm="9">
+                        <Form.Control type="password" placeholder="Password" />
+                        </Col>
+                    </Form.Group>
 
-                <Form.Group as={Row} controlId="formPlaintextPassword">
-                    <Col sm="4">
-                        <Button onClick={signUp} variant="link">Sign up</Button>
-                    </Col>
-                    <Col sm="8">
-                        <Button onClick={signIn} className="btn btn-light w-100">Sign in</Button>
-                    </Col>
-                </Form.Group>
-            </Form>
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Col sm="4">
+                            <Button onClick={signUp} variant="link">Sign up</Button>
+                        </Col>
+                        <Col sm="8">
+                            <Button onClick={signIn} className="btn btn-light w-100">Sign in</Button>
+                        </Col>
+                    </Form.Group>
+                </Form>)
+                : null
+            }
             <hr className="login_form__divisor"/>
             
-            <button type="button" className="login_form__login-with-google-btn" onClick={signIn} >
+            <button type="button" className="login_form__login-with-google-btn mb-3" onClick={signIn} >
                 Sign in with Google
             </button>
+
+            <hr className="login_form__divisor"/>
         </div>
         
     )
